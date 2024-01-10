114ca5a2f8b3c5d9bbc060348ad059d9b6c842de
Rhodri Cusack <rhodricusack@gmail.com>
2024-01-10 08:33:02 +0000

    first commit

---
+++ time_blur.py
@@ -0,0 +1,32 @@
+import torch
+import time
+
+
+def main():
+    nits = 100
+    start = time.time()
+    for it in range(nits):
+        x = torch.randn(1000, 1000, device='cuda')
+        y = torch.randn(1000, 1000, device='cuda')
+        z = torch.matmul(x, y)
+    print ("Method A: Time per iteration: ", (time.time() - start) / nits)
+
+    for it in range(nits):
+        x = torch.randn(1000, 1000).cuda()
+        y = torch.randn(1000, 1000).cuda()
+        z = torch.matmul(x, y)
+    print ("Method B: Time per iteration: ", (time.time() - start) / nits)
+
+    x = torch.randn(nits,1000, 1000).cuda()
+    y = torch.randn(nits,1000, 1000).cuda()
+    z = torch.matmul(x, y)
+    print ("Method C: Time per iteration: ", (time.time() - start) / nits)
+
+    x = [torch.randn(1000, 1000).cuda() for it in range(nits)]
+    y = [torch.randn(1000, 1000).cuda() for it in range(nits)]
+    for it in range(nits):
+        z = torch.matmul(x[it], y[it])
+    print ("Method D: Time per iteration: ", (time.time() - start) / nits)
+
+if __name__ == '__main__':
+    main()
